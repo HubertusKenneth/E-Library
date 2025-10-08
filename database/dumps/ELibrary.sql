1001dc6aa76647505b5f5937f0f4adaf9ef489d3
Kenn <hubertusk26@gmail.com>
2025-10-08 21:28:00 +0700

    update project

--- database/dumps/ELibrary.sql
+++ database/dumps/ELibrary.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 04, 2025 at 12:59 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost
+-- Generation Time: Oct 08, 2025 at 04:00 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `elibrary`
+-- Database: `ELibrary`
 --
 
 -- --------------------------------------------------------
@@ -55,7 +55,10 @@ INSERT INTO `books` (`id`, `title`, `author`, `publisher`, `year`, `genre`, `des
 (8, 'Black Widow: Forever Red', 'Margaret Stohl', 'Marvel Press', '2015', 'Novel', 'A YA novel exploring Black Widow’s past and her new ally.', NULL, '2025-09-22 12:56:42', '2025-09-22 12:56:42'),
 (9, 'Doctor Strange: The Oath', 'Brian K. Vaughan', 'Marvel Comics', '2007', 'Comic', 'Doctor Strange investigates a conspiracy against the medical world.', NULL, '2025-09-22 12:56:42', '2025-09-22 12:56:42'),
 (10, 'Avengers vs. X-Men', 'Various', 'Marvel Comics', '2012', 'Comic', 'The Avengers and the X-Men clash over the fate of the Phoenix Force.', NULL, '2025-09-22 12:56:42', '2025-09-22 12:56:42'),
-(11, 'Huber Ganteng', 'Hubertus Kenneth Alfragisa', 'HKA', '2005', 'Love Yourself', 'Anjay', 'covers/DitDpSeg9t1EDE1kAq0zCUOFQeCrNx7BB11JEctB.jpg', '2025-09-23 08:57:36', '2025-09-23 08:57:36');
+(11, 'Gw Ganteng', 'Hubertus Kenneth Alfragisa', 'HKA', '2005', 'Love Yourself', 'Anjay', 'covers/DitDpSeg9t1EDE1kAq0zCUOFQeCrNx7BB11JEctB.jpg', '2025-09-23 08:57:36', '2025-09-23 08:57:36'),
+(12, 'Halo', 'Hubertus Kenneth Alfragisa', 'HKA', '2000', 'Love Yourself', '-', 'covers/zliMYSulKUsy5bXJDOjETkKaL6jvhYBTpjg49GWl.png', '2025-10-08 02:16:11', '2025-10-08 02:16:11'),
+(13, 'Hubertussssssss', 'Hubertus Kenneth Alfragisa', 'HKA', '2005', 'Love Yourself', 'Wow', 'covers/A2NBSbRXXIYLHDGQk0jOp2FfMP9jTQgW0UJ0G0Nb.png', '2025-10-08 02:25:52', '2025-10-08 02:25:52'),
+(14, 'Shinchan', 'Yoshito Usui', 'ComicsOne', '1990', 'Comedy', NULL, 'covers/adyIgaDMQk3hbvJtIPbPKPcZFRf10EVhmWS2NnYu.jpg', '2025-10-08 06:44:25', '2025-10-08 06:44:25');
 
 -- --------------------------------------------------------
 
@@ -118,7 +121,8 @@ CREATE TABLE `favorites` (
 INSERT INTO `favorites` (`id`, `user_id`, `book_id`, `created_at`, `updated_at`) VALUES
 (6, 2, 1, '2025-09-22 08:36:24', '2025-09-22 08:36:24'),
 (7, 2, 2, '2025-09-22 08:36:26', '2025-09-22 08:36:26'),
-(8, 2, 3, '2025-09-22 09:07:24', '2025-09-22 09:07:24');
+(8, 2, 3, '2025-09-22 09:07:24', '2025-09-22 09:07:24'),
+(11, 1, 2, '2025-09-30 20:02:46', '2025-09-30 20:02:46');
 
 -- --------------------------------------------------------
 
@@ -177,7 +181,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '0001_01_01_000002_create_jobs_table', 1),
 (4, '2025_09_22_110241_create_books_table', 1),
 (5, '2025_09_22_110313_create_favorites_table', 1),
-(6, '2025_09_23_134654_add_role_to_users_table', 2);
+(6, '2025_09_23_134654_add_role_to_users_table', 2),
+(7, '2025_10_08_100000_create_user_activities_table', 3);
 
 -- --------------------------------------------------------
 
@@ -211,8 +216,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('oFo1EpkKFqCCdNhYL8C8mgtQWxKMOb9fDOpkvuGF', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiSjRtek9SbjU0Y0VTQXhTaWdNYzhWZjdxbXlsRE1BMnNHVXVVTTZLUSI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fX0=', 1758685015),
-('vCUoCK08fRpzV9VXmdLElSkeB3Bjy9G68bw4nqrE', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoid2YxVVRSV1F1bGVUazRVSk9sQTljaVl4cGZsMDNDSFRuRWRuUTg1ZyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9ib29rcyI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1759575270);
+('asTz1wzlKseqEQfYIodMyk3oRIgdpiagFj66N4za', 3, '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiNWRkdXJZNFM3cnFqejVmbGRBOXRoZlVsWTRjNXBzWG5EV1J3S2prQiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9ib29rcyI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjM7fQ==', 1759931991);
 
 -- --------------------------------------------------------
 
@@ -239,7 +243,55 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `role`) VALUES
 (1, 'Test01', 'test@test.com', NULL, '$2y$12$r2sA1QGAERzQ8Yh1QTsZU.yVsPcCI.J78pdcgy2xfBzDAQt7cWB6u', NULL, '2025-09-22 05:17:28', '2025-09-22 05:17:28', 'user'),
 (2, 'Kenn', 'hubertusk26@gmail.com', NULL, '$2y$12$2buWkdYn1GsLWC.vfAgNuuaZd0JOc8iv9EWjR/xCkFt.gdBLXbtSC', NULL, '2025-09-22 08:36:04', '2025-09-22 08:36:04', 'user'),
-(3, 'admin', 'admin@library.com', NULL, '$2y$12$KunmWfafjGUxphZpPDtxOuCTuswvEfsNhdsP3mAjq9iUfJkd0lATS', NULL, '2025-09-22 09:51:10', '2025-09-22 09:51:10', 'admin');
+(3, 'admin', 'admin@library.com', NULL, '$2y$12$KunmWfafjGUxphZpPDtxOuCTuswvEfsNhdsP3mAjq9iUfJkd0lATS', NULL, '2025-09-22 09:51:10', '2025-10-08 06:59:19', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_activities`
+--
+
+CREATE TABLE `user_activities` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `action` varchar(255) NOT NULL,
+  `model_type` varchar(255) DEFAULT NULL,
+  `model_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `properties` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`properties`)),
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `user_activities`
+--
+
+INSERT INTO `user_activities` (`id`, `user_id`, `action`, `model_type`, `model_id`, `description`, `properties`, `ip_address`, `user_agent`, `created_at`, `updated_at`) VALUES
+(1, 1, 'profile_update', 'App\\Models\\User', 1, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:35:00', '2025-10-08 06:35:00'),
+(2, 3, 'login', NULL, NULL, 'User logged in', '{\"method\":\"POST\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/login\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:42:45', '2025-10-08 06:42:45'),
+(3, 3, 'book_create', 'App\\Models\\Book', NULL, 'Created new book', '{\"method\":\"POST\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/admin\\/books\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:44:25', '2025-10-08 06:44:25'),
+(4, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:51:35', '2025-10-08 06:51:35'),
+(5, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:51:37', '2025-10-08 06:51:37'),
+(6, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:51:45', '2025-10-08 06:51:45'),
+(7, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:52:03', '2025-10-08 06:52:03'),
+(8, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:53:05', '2025-10-08 06:53:05'),
+(9, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:53:36', '2025-10-08 06:53:36'),
+(10, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:53:48', '2025-10-08 06:53:48'),
+(11, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:54:52', '2025-10-08 06:54:52'),
+(12, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:54:53', '2025-10-08 06:54:53'),
+(13, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:54:57', '2025-10-08 06:54:57'),
+(14, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:57:10', '2025-10-08 06:57:10'),
+(15, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:57:11', '2025-10-08 06:57:11'),
+(16, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:57:12', '2025-10-08 06:57:12'),
+(17, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:57:26', '2025-10-08 06:57:26'),
+(18, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:59:03', '2025-10-08 06:59:03'),
+(19, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:59:05', '2025-10-08 06:59:05'),
+(20, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:59:08', '2025-10-08 06:59:08'),
+(21, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:59:16', '2025-10-08 06:59:16'),
+(22, 3, 'profile_update', 'App\\Models\\User', 3, 'Updated profile information', '{\"method\":\"PATCH\",\"url\":\"http:\\/\\/127.0.0.1:8000\\/profile\",\"actual_method\":\"PATCH\"}', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36', '2025-10-08 06:59:19', '2025-10-08 06:59:19');
 
 --
 -- Indexes for dumped tables
@@ -319,6 +371,15 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
+-- Indexes for table `user_activities`
+--
+ALTER TABLE `user_activities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_activities_user_id_created_at_index` (`user_id`,`created_at`),
+  ADD KEY `user_activities_action_created_at_index` (`action`,`created_at`),
+  ADD KEY `user_activities_model_type_model_id_index` (`model_type`,`model_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -326,7 +387,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -338,7 +399,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `favorites`
 --
 ALTER TABLE `favorites`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `jobs`
@@ -350,13 +411,19 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_activities`
+--
+ALTER TABLE `user_activities`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
@@ -368,6 +435,12 @@ ALTER TABLE `users`
 ALTER TABLE `favorites`
   ADD CONSTRAINT `favorites_book_id_foreign` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `favorites_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_activities`
+--
+ALTER TABLE `user_activities`
+  ADD CONSTRAINT `user_activities_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
