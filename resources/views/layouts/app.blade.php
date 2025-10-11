2557e329ca77b9d4c3d5d5c015cf9f3cdd7fd5b7
Kenn <hubertusk26@gmail.com>
2025-10-11 18:16:07 +0700

    apply bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>E-Library</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
   @vite(['resources/css/app.css','resources/js/app.js'])
   <script src="//unpkg.com/alpinejs" defer></script>
 </head>
@@ -42,17 +45,10 @@
         </div>
       </div>
 
-      {{-- <span 
-        class="block px-4 py-2 rounded text-gray-400 cursor-not-allowed"
-        title=""
-      >
-        Categories
-      </span> --}}
-
       <a href="{{ route('categories.index') }}" 
            class="block px-4 py-2 rounded hover:bg-slate-100">
             Categories
-        </a>
+      </a>
       
       @auth
         <a href="{{ route('favorites.index') }}" 
@@ -128,5 +124,7 @@
     </main>
   </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
